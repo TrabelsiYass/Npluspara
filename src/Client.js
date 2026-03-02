65737ade822d58f088b3255a7c35045a4525e8cb
Trabelsi Yassin <82684673+TrabelsiYass@users.noreply.github.com>
2026-03-02 06:35:19 +0100

    Update Client.js

--- src/Client.js
+++ src/Client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://fvujrqrurgyzdfxodrds.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2dWpycXJ1cmd5emRmeG9kcmRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MjczNjYsImV4cCI6MjA4NjQwMzM2Nn0.iqABJ61aEmCWpuHNXSaWLdlef0dfjOXfpqsR6tnltX0'
+const supabaseUrl = ''
+const supabaseAnonKey = ''
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
