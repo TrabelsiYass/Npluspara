d5f18f13e1993ae1b6e5c79aadea1fdd91115bf9
Yass Trabelsi <trabelsiyassin32@gmail.com>
2026-05-25 13:52:38 +0100

    Fix homepage freezing, lazy loading, and build warning fixes 3

--- src/Components/HomeBanner/index.js
+++ src/Components/HomeBanner/index.js
@@ -1,25 +1,65 @@
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination'; // Added pagination CSS
-import { Autoplay, Navigation, Pagination } from 'swiper/modules'; // Added Pagination
+import 'swiper/css/pagination'; 
+import { Autoplay, Navigation, Pagination } from 'swiper/modules'; 
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Desktop Banners
 import banner1 from '../../assets/images/banner1.png';
 import banner2 from '../../assets/images/banner3.jpeg';
 import banner3 from '../../assets/images/banner4.jpeg';
 import banner4 from '../../assets/images/banner5.png';
-import banner5 from '../../assets/images/banner6.png';
 
+// Mobile Banners
+import mobileview1 from '../../assets/images/mobilebanner1.png'; 
+import mobileview2 from '../../assets/images/mobilebanner2.png';
+import mobileview3 from '../../assets/images/mobilebanner3.png';
+import mobileview4 from '../../assets/images/mobilebanner4.png';
+
+import { Link } from 'react-router-dom';
 import './index.css';
 
 const HomeBanner = () => {
+    // Array mapping ensures each desktop banner connects smoothly to its mobile version
+    const banners = [
+        { 
+            id: 1, 
+            desktop: banner1, 
+            mobile: mobileview1, 
+            alt: "Summer Promo",
+            link: "/category/protection-solaire"
+        },
+        { 
+            id: 2, 
+            desktop: banner2, 
+            mobile: mobileview2, 
+            alt: "Compléments Alimentaires",
+            link: "/category/aromatherapie-et-huiles-essentielles"
+        },
+        { 
+            id: 3, 
+            desktop: banner3, 
+            mobile: mobileview3, 
+            alt: "Anniversaire Promo",
+            link: "/category/bons-plans"
+        },
+        { 
+            id: 4, 
+            desktop: banner4, 
+            mobile: mobileview4, 
+            alt: "Soins Bébé",
+            link: "/category/maman-bebe/soins-du-bebe"
+        },
+    ];
+
     return (
         <section className="HomeBanner">
-            <div className="container-fluid"> {/* Use a container to keep it aligned */}
+            <div className="container-fluid"> 
                 <Swiper
                     slidesPerView={1}
-                    spaceBetween={0} // Banners usually have 0 space for a seamless look
+                    spaceBetween={0} 
                     navigation={true}
-                    loop={true} // Banners are better looped
+                    loop={true} 
                     autoplay={{
                         delay: 3500,
                         disableOnInteraction: false,
@@ -30,49 +70,25 @@ const HomeBanner = () => {
                     modules={[Navigation, Autoplay, Pagination]}
                     className="mySwiper"
                 >
-                    
-                        
-                        
-                        <SwiperSlide>
+                    {banners.map((banner) => (
+                        <SwiperSlide key={banner.id}>
                             <div className='item'>
-                                <img 
-                                    src={banner1}
-                                    loading="lazy" 
-                                    className='w-100' 
-                                    alt="banner1" 
-                                />
+                                <Link to={banner.link} className='banner-link'>
+                                <picture>
+                                    {/* Displays mobile banner on viewports 576px wide or smaller */}
+                                    <source media="(max-width: 576px)" srcSet={banner.mobile} />
+                                    {/* Default desktop fallback layout */}
+                                    <img 
+                                        src={banner.desktop} 
+                                        loading="lazy" 
+                                        className='w-100' 
+                                        alt={banner.alt} 
+                                    />
+                                </picture>
+                                </Link>
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide>
-                            <div className='item'>
-                                <img 
-                                    src={banner2}
-                                    loading="lazy" 
-                                    className='w-100' 
-                                    alt="banner1" 
-                                />
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide >
-                            <div className='item'>
-                                <img src={banner3} loading="lazy" className='w-100' alt="banner" />
-                            </div>
-                        </SwiperSlide>
-
-                        <SwiperSlide >
-                            <div className='item'>
-                                <img src={banner4} loading="lazy" className='w-100' alt="banner" />
-                            </div>
-                        </SwiperSlide>
-
-                         <SwiperSlide >
-                            <div className='item'>
-                                <img src={banner5}loading="lazy" className='w-100' alt="banner" />
-                            </div>
-                        </SwiperSlide>
-
-                        
-                  
+                    ))}
                 </Swiper>
             </div>
         </section>
